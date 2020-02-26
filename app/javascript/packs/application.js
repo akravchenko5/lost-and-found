f1c84ca2aa484c11548de967abb2c81ef89efadd
Simen Grytoyr <simen.grytoyr@gmail.com>
2020-02-26 18:39:45 +0100

    profile page in going

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,11 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
-import { createItem, foundNew } from '../components/init_create_item'
+import { createItem, foundNew, displaySettings } from '../components/init_create_item'
 import { count } from '../plugins/init_counter';
 
 initMapbox();
 createItem();
 foundNew();
 count();
+displaySettings();
