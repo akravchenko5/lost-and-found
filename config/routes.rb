4184d6ffc47a5ea2c3a035fa57142d1a5ed31d77
Julian Giertsen da Cunha <juliangdacunha@gmail.com>
2020-02-27 13:19:11 +0100

    new message and create method + show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'reviews/show'
+  get 'reviews/index'
+  get 'reviews/edit'
+  get 'reviews/update'
+  get 'reviews/delete'
+  get 'reviews/destroy'
   root to: 'items#home'
   get 'items/map', to: 'items#map', as: :map
   resources :items, except: :home do
@@ -10,5 +18,9 @@ Rails.application.routes.draw do
     resources :conversations, only: [:create]
   end
   devise_for :users
+   resources :users, only: [:show] do
+    resources :reviews, only: [:new, :create, :index]
+    end
+    resources :reviews, only: [:show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
