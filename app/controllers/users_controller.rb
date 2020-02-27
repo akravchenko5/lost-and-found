f375184988ad6abfac6014a44313fd217e173dec
Simen Grytoyr <simen.grytoyr@gmail.com>
2020-02-27 17:45:26 +0100

    working with reviews view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @user_rating = average_rating(@user)
+  end
+
+  private
+
+  def average_rating(user)
+    sum = 0.0
+
+    @user.reviews_as_reviewed.each do |review|
+      sum += review.rating
+    end
+    rating = (sum / @user.reviews_as_reviewed.count).to_f
+    (rating * 2.0).round / 2.0
   end
 end
 
