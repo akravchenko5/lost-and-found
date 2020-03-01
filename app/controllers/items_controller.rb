6514d6c584fa21dcbe034865cbfc755cf872c426
Aleksandr Kravchenko <a.kravchenko.intek@gmail.com>
2020-03-01 12:37:49 +0100

    search form functionality finished

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,11 +18,31 @@ class ItemsController < ApplicationController
     # raise
     if search_terms
       # json_hits = Item.search.raw_answer.with_indifferent_access[:hits]
-      @items = Item.search(search_terms, {
+      items = Item.search(search_terms, {
         aroundLatLngViaIP: true,
-        aroundRadius: @radius
+        aroundRadius: @radius,
+      })
 
-         })
+      date_filter(items)
+
+      # search_item = params[:query]
+      # if !search_item[:start_date].blank? && !search_item[:stop_date].blank?
+      #   @items = items.select { |item|
+      #     item.created_at >  search_item[:start_date].to_date && item.created_at <  search_item[:stop_date].to_date
+      #   }
+      # elsif search_item[:start_date] && search_item[:stop_date].blank?
+      #   raise
+      #   @items = items.select { |item|
+      #     item.created_at >  search_item[:start_date].to_date
+      #   }
+      # elsif search_item[:start_date].blank? && search_item[:stop_date]
+      #   raise
+      #   @items = items.select { |item|
+      #     item.created_at <  search_item[:stop_date].to_date
+      #   }
+      # else
+      #   @items = items
+      # end
       # ip = Ip::Lookup.server_whatismyipaddress
       ip = "193.214.55.86" #for development
       @location = Geocoder.search(ip).first.coordinates
@@ -106,6 +126,25 @@ class ItemsController < ApplicationController
       search_item[:category],
       search_item[:state]
     ].compact
+  end
+
+  def date_filter(items)
+    search_item = params[:query]
+    if !search_item[:start_date].blank? && !search_item[:stop_date].blank?
+      @items = items.select { |item|
+        item.created_at >  search_item[:start_date].to_date && item.created_at <  search_item[:stop_date].to_date
+      }
+    elsif search_item[:start_date] && search_item[:stop_date].blank?
+      @items = items.select { |item|
+        item.created_at >  search_item[:start_date].to_date
+      }
+    elsif search_item[:start_date].blank? && search_item[:stop_date]
+      @items = items.select { |item|
+        item.created_at <  search_item[:stop_date].to_date
+      }
+    else
+      @items = items
+    end
   end
 
   def set_item
