839e09eeb326885ac419d293822cc85e70819552
Aleksandr Kravchenko <a.kravchenko.intek@gmail.com>
2020-03-02 17:35:56 +0100

    typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://https://www.lostfound.to/cable'
+  config.action_cable.url = 'wss://www.lostfound.to/cable'
   config.action_cable.allowed_request_origins = [ 'https://www.lostfound.to', /https:\/\/lostfound.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
