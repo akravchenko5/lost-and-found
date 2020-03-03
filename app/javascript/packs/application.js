d74d3a388e7517785cdb0f0d01771494a5ae034b
Aleksandr Kravchenko <a.kravchenko.intek@gmail.com>
2020-03-03 09:51:32 +0100

    working on autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,11 @@ require("jquery")
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js/es'
+import { searchBox, hits } from 'instantsearch.js/es/widgets';
+import { connectAutocomplete } from 'instantsearch.js/es/connectors';
+
 import { initMapbox } from '../plugins/init_mapbox';
 import { createItem, foundNew, displaySettings } from '../components/init_create_item';
 import { toggleSettings } from '../components/init_toggle_settings';
@@ -17,7 +22,7 @@ import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initSlider } from '../plugins/init_slider';
 import { fetchCurrentPosition } from '../components/init_current_location';
 import { subscribeConversations } from '../plugins/init_conversation';
-// import { searchAutocomplete } from '../plugins/algolia';
+import { searchAutocomplete } from '../plugins/algolia';
 
 initMapbox();
 createItem();
@@ -32,7 +37,7 @@ initSlider();
 subscribeConversations();
 
 fetchCurrentPosition();
-// searchAutocomplete();
+searchAutocomplete();
 scrolledDown();
 
 
--- app/javascript/plugins/algolia.js
+++ app/javascript/plugins/algolia.js
@@ -1,43 +1,99 @@
-import algoliasearch from 'algoliasearch';
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js/es'
+import { searchBox, hits } from 'instantsearch.js/es/widgets';
+import { connectAutocomplete } from 'instantsearch.js/es/connectors';
+
 
 const searchAutocomplete = () => {
 
+const searchClient = algoliasearch('1LWVV3TZRZ', '344b5af8f379dac1979f13b494b4578a');
 
-const client = algoliasearch('1LWVV3TZRZ', '696ddfef9346874983dd1d2ec842b9bd');
-const index = client.initIndex('items');
-index.setSettings({
-  searchableAttributes: ['title'],
-  customRanking: ['desc(points)'],
+const search = instantsearch({
+  indexName: 'items',
+  searchClient,
 });
-// const teams = client.initIndex('teams');
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#autocomplete',
+  }),
+
+  instantsearch.widgets.hits({
+    container: '#hits',
+  })
+]);
+
+search.start();
 
 
-autocomplete(
-    '#aa-search-input',
-    {
-      debug: true,
-      templates: {
-        dropdownMenu:
-          '<div class="aa-dataset-player"></div>' +
-          '<div class="aa-dataset-team"></div>',
-      },
-    },
-    [
-      {
-        source: autocomplete.sources.hits(items, {hitsPerPage: 7}),
-        displayKey: 'title',
-        name: 'item',
-        templates: {
-          header: '<div class="aa-suggestions-category">items</div>',
-          suggestion({_highlightResult}) {
-            return `<span>${_highlightResult.title.value}</span><span>${_highlightResult.address.value}</span>`;
-          },
-          empty: '<div class="aa-empty">No matching items</div>',
-        },
-      }
-    ]
-  );
-};
+
+// const search = instantsearch({
+//   indexName: 'items',
+//   searchClient: algoliasearch(
+//   '1LWVV3TZRZ',
+//   '344b5af8f379dac1979f13b494b4578a'
+//   ),
+// });
+
+// search.start();
+
+// // Helper for the render function
+//   const renderIndexListItem = ({ indexId, hits }) => `
+//     <li>
+//       Index: ${indexId}
+//       <ol>
+//         ${hits
+//           .map(
+//             hit =>
+//               `<li>${instantsearch.highlight({ attribute: 'title', hit })}</li>`
+//           )
+//           .join('')}
+//       </ol>
+//     </li>
+//   `;
+
+//   // Create the render function
+//   const renderAutocomplete = (renderOptions, isFirstRender) => {
+//     const { indices, currentRefinement, refine, widgetParams } = renderOptions;
+
+//     if (isFirstRender) {
+//       const input = document.createElement('input');
+//       const ul = document.createElement('ul');
+
+//       input.addEventListener('input', event => {
+//         refine(event.currentTarget.value);
+//       });
+
+//       widgetParams.container.appendChild(input);
+//       widgetParams.container.appendChild(ul);
+//     }
+
+//     widgetParams.container.querySelector('input').value = currentRefinement;
+//     widgetParams.container.querySelector('ul').innerHTML = indices
+//       .map(renderIndexListItem)
+//       .join('');
+//   };
+
+//   // Create the custom widget
+//   // const customAutocomplete = instantsearch.connectors.connectAutocomplete(
+//   //   renderAutocomplete
+//   // );
+//   const customAutocomplete = connectAutocomplete(
+//     renderAutocomplete
+//   );
+
+//   // const autocomplete = connectAutocomplete(renderer);
+//   // Instantiate the custom widget
+//   search.addWidgets([
+//     index({ indexName: 'instant_search_title_asc' }),
+//     // index({ indexName: 'instant_search_price_desc' }),
+
+//     customAutocomplete({
+//       container: document.querySelector('#autocomplete'),
+//     })
+//   ]);
+
+}
 
 export { searchAutocomplete }
 
